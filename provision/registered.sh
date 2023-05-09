08a5c6b442eeee199ef59896ca6c2fc19505d588
Edwin Bello <ayiembaeb@gmail.com>
2023-05-09 16:12:28 +0300

    test script

--- provision/registered.sh
+++ provision/registered.sh
@@ -2,8 +2,8 @@
 
 curl --location --request PUT 'http://localhost:7557/provisions/oneisp-registered' \
 --data-raw 'const now = Date.now();
-declare("InternetGatewayDevice.ManagementServer.ConnectionRequestUsername", { value: now }, { value: "$ConnectionRequestUsername" });
-declare("InternetGatewayDevice.ManagementServer.ConnectionRequestPassword", { value: now }, { value: "$ConnectionRequestPassword" })
+declare("InternetGatewayDevice.ManagementServer.ConnectionRequestUsername", { value: now }, { value: "'"$ConnectionRequestUsername"'" });
+declare("InternetGatewayDevice.ManagementServer.ConnectionRequestPassword", { value: now }, { value: "'"$ConnectionRequestPassword"'" })
 
 let serialNumber = declare("DeviceID.SerialNumber", { value: 1 }).value[0];
 let productClass = declare("DeviceID.ProductClass", { value: 1 }).value[0];
